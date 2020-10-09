076e647d068f71c00f67c1f960555fc02b9aeffd
kareemgunner <67743472+kareemgunner@users.noreply.github.com>
2020-10-09 21:28:28 +0200

    Add files via upload

--- Assignment5/Question13_NesteBW.java
+++ Assignment5/Question13_NesteBW.java
@@ -17,13 +17,37 @@ public class Question13_NesteBW {
 //		1. Total 8 rows and 8 columns.
 //		2. "W" and "B" are in alternate positions (row-wise or column-wise), you will not find consecutive W or B.
 		
-		for (int i=1; i<=4; i++) {
-			for (int j=1; j<=4; j++) {
-			System.out.print("W B ");
-			}System.out.println();
-			for (int j=1; j<=4; j++) {
-				System.out.print("B W ");
-				}System.out.println();
+//		for (int i=1; i<=4; i++) {
+//			for (int j=1; j<=4; j++) {
+//			System.out.print("W B ");
+//			}System.out.println();
+//			for (int j=1; j<=4; j++) {
+//				System.out.print("B W ");
+//				}System.out.println();
+//		}
+		
+		for (int k=1; k<=4; k++) {
+			for (int i=1; i<=4; i++) {
+			
+				for (int j=1; j<=1; j++) {
+				System.out.print("W ");
+				}
+				for (int j=1; j<=1; j++) {
+					System.out.print("B ");
+				}
+			}	
+			System.out.println();
+			for (int i=1; i<=4; i++) {
+				
+				for (int j=1; j<=1; j++) {
+				System.out.print("B ");
+				}
+				for (int j=1; j<=1; j++) {
+					System.out.print("W ");
+				}
+			}	
+			System.out.println();
+		
 		}
 
 	}
